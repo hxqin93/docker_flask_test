58dfab3767c2951d7b220b8f98453a580d9655f7
Haoxiang <haoxiang.qin@aihuishou.com>
2020-01-15 16:21:24 +0800

    日志输出

--- app/hello.py
+++ app/hello.py
@@ -1,20 +1,20 @@
 from flask import Flask
 import logging
 app = Flask(__name__)
-
+from log import logger
 
 @app.route('/')
 def hello():
-    app.logger.debug('this is debug message')
-    app.logger.error('this is error message')
-    app.logger.info('this is info message')
+    # logger.debug('this is debug message')
+    logger.error('this is error message')
+    logger.info('this is info message')
     return 'hello,world'
 
 
-if __name__ == '__main__':
-    app.run()
-
-if __name__ != '__main__':
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
+# if __name__ == '__main__':
+#     app.run()
+#
+# if __name__ != '__main__':
+#     gunicorn_logger = logging.getLogger('gunicorn.error')
+#     app.logger.handlers = gunicorn_logger.handlers
+#     app.logger.setLevel(gunicorn_logger.level)
